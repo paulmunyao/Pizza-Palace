191b4465f5528b76877eb844b0f939e2e3345171
Paul Munyao <paulmunyao094@gmail.com>
2022-01-17 13:14:28 +0300

    Determine price for small pizza cracked crust

--- index.js
+++ index.js
@@ -1,25 +1,27 @@
 jQuery(function () {
-    jQuery("summary").hide();
-    jQuery(".cdata-overlay").hide();
+  jQuery("summary").hide();
+  jQuery(".cdata-overlay").hide();
 
-    jQuery("#checkout").click(function () {
-        let size = jQuery("#size option:selected").val();
-        let crust = jQuery("#crust option:selected").val();
-        let topping = jQuery("#topping option:selected").val();
-        let number = jQuery("#number").val();
+  jQuery("#checkout").click(function () {
+    let size = jQuery("#size option:selected").val();
+    let crust = jQuery("#crust option:selected").val();
+    let topping = jQuery("#topping option:selected").val();
+    let number = jQuery("#number").val();
 
-        function order(s, c, t, n, total) {
-          return {s, c, t, n, total};
-      }
-    });
-
-    let price;
-    let totalPrice;
-    switch (size){
-        case size = "small":
-            price = 300;
-            if (crust === "stuffed crust"){
-                totalPrice = (price * number) + 200;
-            }
+    function order(s, c, t, n, total) {
+      return { s, c, t, n, total };
     }
+  });
+
+  let price;
+  let totalPrice;
+  switch (size) {
+    case (size = "small"):
+      price = 300;
+      if (crust === "stuffed crust") {
+        totalPrice = price * number + 200;
+      } else if (crust === "cracked crust") {
+        totalPrice = price * number + 250;
+      }
+  }
 });
