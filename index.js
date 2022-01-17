a93ffcb601da9ffc9268d31d644a9bc84f58957a
Paul Munyao <paulmunyao094@gmail.com>
2022-01-17 12:04:03 +0300

    Add class let

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
-$(function () {
-  $("summary").hide();
-  $(".cdata-overlay").hide();
+jQuery(function () {
+  jQuery("summary").hide();
+  jQuery(".cdata-overlay").hide();
 
-  
+  jQuery("#checkout").click(function () {
+      let size = jQuery("#size option:selected").val();
+      let crust = jQuery("#crust option:selected").val();
+      let topping = jQuery("#topping option:selected").val();
+      let number = jQuery("#number").val();
+  });
 });
