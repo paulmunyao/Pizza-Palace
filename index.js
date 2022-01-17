95b5820708da720766cd06f8b0b2e445c4699327
Paul Munyao <paulmunyao094@gmail.com>
2022-01-17 12:45:02 +0300

    Get price

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 jQuery(function () {
-  jQuery("summary").hide();
-  jQuery(".cdata-overlay").hide();
+    jQuery("summary").hide();
+    jQuery(".cdata-overlay").hide();
 
-  jQuery("#checkout").click(function () {
-      let size = jQuery("#size option:selected").val();
-      let crust = jQuery("#crust option:selected").val();
-      let topping = jQuery("#topping option:selected").val();
-      let number = jQuery("#number").val();
+    jQuery("#checkout").click(function () {
+        let size = jQuery("#size option:selected").val();
+        let crust = jQuery("#crust option:selected").val();
+        let topping = jQuery("#topping option:selected").val();
+        let number = jQuery("#number").val();
 
-      function order(s, c, t, n, total) {
-        return {s, c, t, n, total};
-    }
-  });
+        function order(s, c, t, n, total) {
+          return {s, c, t, n, total};
+      }
+    });
+
+    let price;
+    let totalPrice;
+    switch (size)
 });
